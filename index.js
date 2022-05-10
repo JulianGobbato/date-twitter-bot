fee2e8edf5b79dee4b3f9553e1daed382d536f1a
JulianGobbato <juliangobbato@hotmail.com>
2022-05-10 20:02:19 -0300

    TwBot rdy

--- index.js
+++ index.js
@@ -8,27 +8,35 @@ const client = new TwitterApi({
     accessSecret: accessSecret,
   });
 
-async function postTw() {
-    const mediaId = await client.v1.uploadMedia('img/photo.jpg');
-    client.v2.tweet('Hello, this is a test.', {media: { media_ids: [mediaId]}} );
+async function postTw(img, text='') {
+    const mediaId = await client.v1.uploadMedia(img);
+    client.v2.tweet(text, {media: { media_ids: [mediaId]}} );
 }
 
-postTw()
-//function loop(){
-//  setInterval(mandarMensaje, 1000)
-//}
-//
-//loop()
+function loop(){
+  setInterval(checkDate, 1000*60*60*24)
+}
 
-//function checkDate(){
-//  let urls = [];
-//  let today = new Date
-//  if(today.getDay() == 3){
-//    urls.push('https://pbs.twimg.com/media/EMrbNaJXYAAuCgb.jpg')
-//  }
-//  if(today.getDay() == 5){
-//    urls.push('')
-//  }
-//}
-//
-//checkDate()
+function checkDate(){
+
+  let today = new Date
+  if(today.getDay() == 1) { postTw('img/f6.jpg',); }
+  if(today.getDay() == 3) {
+    postTw('img/f5.jpg');
+    postTw('img/f1.jpg');
+  }
+  if(today.getDay() == 4) { postTw('img/f2.jpg'); }
+  if(today.getDay() == 5) { postTw('img/v1.mp4'); }
+  if(today.getDate() == 2 && today.getMonth() == 1) { postTw('img/f4.jpg'); }
+  if(today.getDate() == 14 && today.getMonth() == 3) { postTw('img/f11.jpg'); }
+  if(today.getDate() == 15 && today.getMonth() == 3) { postTw('img/f10.jpg'); }
+  if(today.getDate() == 2 && today.getMonth() == 5) { postTw('img/f3.jpg'); }
+  if(today.getDate() == 5 && today.getMonth() == 5) { postTw('img/f14.jpg'); }
+  if(today.getDate() == 25 && today.getMonth() == 5) { postTw('img/f8.jpg'); }
+  if(today.getDate() == 10 && today.getMonth() == 8) { postTw('img/f12.jpg', 'Happy birthday Peter Parker'); }
+  if(today.getDate() == 11 && today.getMonth() == 10) { postTw('img/f7.jpg'); }
+  if(today.getDate() == 5 && today.getMonth() == 11) { postTw('img/f9.jpg'); }
+  if(today.getDate() == 15 && today.getMonth() == 11) { postTw('img/f9.jpg'); }
+}
+
+loop()
